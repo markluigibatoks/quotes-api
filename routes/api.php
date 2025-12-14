4922b8c33b39ce155515f88ac550138fa7346640
Mark-Luigi <markluigibatoks@gmail.com>
2025-12-14 09:53:35 -0330

    add Quotes model

---
+++ routes/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\QuoteController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('quotes', [QuoteController::class, 'index']);
+
+
+// [
+//   {
+//     "quote": "Push yourself because no one else is going to do it for you.",
+//     "author": "Unknown"
+//   },
+//   {
+//     "quote": "No pain, no gain.",
+//     "author": "Benjamin Franklin"
+//   },
+//   {
+//     "quote": "Don’t limit your challenges. Challenge your limits.",
+//     "author": "Jerry Dunn"
+//   },
+//   {
+//     "quote": "The body achieves what the mind believes.",
+//     "author": "Napoleon Hill"
+//   },
+//   {
+//     "quote": "Sweat is fat crying.",
+//     "author": "Unknown"
+//   },
+//   {
+//     "quote": "Your only limit is you.",
+//     "author": "Unknown"
+//   },
+//   {
+//     "quote": "Strength does not come from winning. Your struggles develop your strengths.",
+//     "author": "Arnold Schwarzenegger"
+//   },
+//   {
+//     "quote": "Excuses don’t burn calories.",
+//     "author": "Unknown"
+//   },
+//   {
+//     "quote": "Great things never come from comfort zones.",
+//     "author": "Unknown"
+//   },
+//   {
+//     "quote": "Train insane or remain the same.",
+//     "author": "Unknown"
+//   }
+// ]

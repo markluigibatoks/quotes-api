655647ca6281e6880fe2b5fce727ebb579a172e6
Mark-Luigi <markluigibatoks@gmail.com>
2026-01-24 08:33:28 -0330

    add: matchingpair route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MatchingPairController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('quotes', QuoteController::class)->only(['index', 'show']);
+Route::apiResource('matching-pair', MatchingPairController::class)->only(['index', 'show']);
 
 
 // [

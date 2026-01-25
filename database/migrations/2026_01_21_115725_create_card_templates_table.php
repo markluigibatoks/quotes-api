8bf52325b03130e3baeb4876960dffefadfb13ac
Mark-Luigi <markluigibatoks@gmail.com>
2026-01-24 21:02:09 -0330

    add: CardTemplate table

--- database/migrations/2026_01_21_115725_create_card_templates_table.php
+++ database/migrations/2026_01_21_115725_create_card_templates_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cards', function (Blueprint $table) {
+        Schema::create('card_templates', function (Blueprint $table) {
             $table->id();
-            $table->integer('pair_id');
             $table->string('value');
-            $table->foreignId('matching_pair_id')->constrained()->cascadeOnDelete();
+            $table->string('category');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cards');
+        Schema::dropIfExists('card_templates');
     }
 };
